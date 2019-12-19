e2e95dcf8a50627fdba6f2c86ee155099ccd031c
nadineharsch <nadine.harsch@uni-ulm.de>
2019-12-19 14:37:32 +0100

    Kommentare einfügen

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
-import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
-    //Intervalle haben die Form [kleinste Zahl, groesste Zahl]
+    //Intervals have the form [smallest number, largest number]
 
+    /**
+     *
+     * @param args
+     */
     public static void main (String[] args) {
-        //leere Liste erzeugen
+        //create an empty list
         List<int[]> inputlist = new ArrayList<int[]>();
 
-        //Intervalle einlesen
+        //Read in Intervals from terminal
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         String [] temp = input.split(" ");
@@ -23,32 +26,40 @@ public class Main {
             inputlist.add(arr);
         }
 
-        // Testausgabe wegen Eingabe
-//        for(int j = 0; j < inputlist.size(); j++){
-//            System.out.println("Intervall " + (j+1) + ": [" + inputlist.get(j)[0] + ", " + inputlist.get(j)[1] + "]");
-//        }
-
-        //Mergefunktion auf Liste anwenden
+        //Apply merge function to list
         List<int[]> ergebnis = merge(inputlist);
         for(int j = 0; j < ergebnis.size(); j++){
             System.out.print("[" + ergebnis.get(j)[0] + ", " + ergebnis.get(j)[1] + "] ");
         }
     }
 
+    /**
+     *
+     * @param list
+     * @return
+     */
+    //merges overlapping intervals into one interval
     public static List<int[]> merge (List<int[]> list) {
         for(int i = 0; i < list.size(); i++){
             for(int j = 0; j < list.size(); j++){
+                // checks if the indices are unequal and checks if the negated statement (end of the first interval is
+                // smaller than the start of the second interval or the start of the first interval is larger than the
+                // end of the second interval) is true
                 if(!((list.get(i))[1] < (list.get(j))[0] || (list.get(i))[0] > (list.get(j))[1]) && i != j){
+                   // check and decide which value has to be replaced by which one
                     if(list.get(i)[0] > list.get(j)[0]){
                         list.get(i)[0] = list.get(j)[0];
                     }
                     if(list.get(i)[1] < list.get(j)[1]){
                         list.get(i)[1] = list.get(j)[1];
                     }
+                    // removes redundant interval
                     list.remove(j);
                 }
             }
         }
+
+        //sorts the arrays created after using the merge function
         Collections.sort(list, new Comparator<int[]>() {
             @Override
             public int compare(int[] a1, int[] a2) {
